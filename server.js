90da3ac486a0315caaf91f2f4cbd0997295b1175
mir-sakawath-ahmed <241006712@eastdelta.edu.bd>
2026-04-11 00:40:20 +0600

    Fix - add all backend files

--- server.js
+++ server.js
@@ -41,3 +41,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("DB Error:", err));
