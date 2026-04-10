7ae1858ad91e54fab1313d21b7932ae9a7c2ecc5
mir-sakawath-ahmed <241006712@eastdelta.edu.bd>
2026-04-10 23:51:51 +0600

    fix model import case issue

--- routes/services.js
+++ routes/services.js
@@ -5,10 +5,10 @@ const {
 } = require("../controllers/serviceController");
 const { protect, adminOnly } = require("../middleware/auth");
 
+
 router.get("/", getAllServices);
 router.get("/:id", getServiceById);
 router.post("/", protect, adminOnly, createService);
 router.put("/:id", protect, adminOnly, updateService);
 router.delete("/:id", protect, adminOnly, deleteService);
-
 module.exports = router;
